7ca6253cee4ae07849166e9076835b6267cae548
9bugs <13419612520@139.com>
2020-09-08 14:44:01 +0800

    add spring cloud config support

---
+++ configclient/src/main/java/com/yibs/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.yibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liwh
+ * @title: ConfigClientApplication
+ * @projectName mySpringCloud
+ * @description: TODO
+ * @date 2020/9/8 14:01
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
